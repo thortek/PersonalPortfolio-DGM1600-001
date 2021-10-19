afdc752e4bc7a5c704fb48ca14e7696aa7b0a736
Thor Anderson <ta.anderson@gmail.com>
2021-10-19 10:51:09 -0600

    Done with films for now

--- films/sw-films.js
+++ films/sw-films.js
@@ -2,19 +2,16 @@ import { films } from '../data/films.js'
 
 let filmList = document.querySelector('#filmList')
 
-filmList.textContent = 'This is my content. Here is some more content.'
-
-let myImg = document.createElement('img')
-myImg.src = `https://starwars-visualguide.com/assets/img/films/1.jpg`
-
-filmList.appendChild(myImg)
-
-let titleList = document.createElement('ol')
-filmList.appendChild(titleList)
-
 for (let i = 0; i < films.length; i++) {
-    console.log(films[i].title);
-    let listItem = document.createElement('li')
-    listItem.textContent = films[i].title
-    titleList.appendChild(listItem)
+  let figure = document.createElement('figure')
+  let figImg = document.createElement('img')
+  figImg.src = `https://starwars-visualguide.com/assets/img/films/${i + 1}.jpg`
+  let figCaption = document.createElement('figcaption')
+  figCaption.textContent = films[i].title
+  
+  figure.appendChild(figImg)
+  figure.appendChild(figCaption)
+  filmList.appendChild(figure)
  }
+
+ // add a function later that will get the URL property and use Array.find() to match titles with posters
