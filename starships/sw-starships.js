cfb9205d88c7a17bd0a68946196afa83d9effd59
Thor Anderson <ta.anderson@gmail.com>
2021-10-28 11:16:19 -0600

    Got started on starships

---
+++ starships/sw-starships.js
@@ -0,0 +1,29 @@
+import { starships } from "../data/starships.js";
+
+const nav = document.querySelector('.nav')
+const navList = document.querySelector('.navList')
+const shipView = document.querySelector('.displaySection')
+
+function populateNav(starships) {
+  starships.forEach(starship => {
+    let anchorWrap = document.createElement('a')
+    anchorWrap.href = '#'
+    let listItem  = document.createElement('li')
+    listItem.textContent = starship.name
+    anchorWrap.addEventListener('click', () => {
+      populateShipView(starship)
+    })
+
+    anchorWrap.appendChild(listItem)
+    navList.appendChild(anchorWrap)
+  })
+}
+
+populateNav(starships)
+
+function populateShipView(shipData) {
+  console.log(`Thanks for clicking on ${shipData.name}`)
+  let shipImage = document.createElement('img')
+  shipImage.src = `https://starwars-visualguide.com/assets/img/starships/15.jpg`
+  shipView.appendChild(shipImage)
+}
