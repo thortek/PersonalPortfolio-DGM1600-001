1f097212c27c5215ad8dc59f64d9cc8d4abe7da3
Thor Anderson <ta.anderson@gmail.com>
2021-11-11 11:17:21 -0700

    Beautiful reduce function!

--- congress/congress.js
+++ congress/congress.js
@@ -2,13 +2,15 @@ import { senators } from '../data/senators.js'
 
 const senatorDiv = document.querySelector('.senators')
 
-function SimplifiedSenators(senatorArray) {
-  return senatorArray.map(senator => {
+function SimplifiedSenators() {
+  return senators.map(senator => {
     let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
     return {
       id: senator.id,
       name: `${senator.first_name}${middleName}${senator.last_name}`,
       party: senator.party,
+      gender: senator.gender,
+      seniority: +senator.seniority,
       imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`
     }
   })
@@ -29,4 +31,12 @@ function populateSenatorDiv(simpleSenators) {
   })
 }
 
-populateSenatorDiv(SimplifiedSenators(senators))
+const filterSenators = (prop, value) => SimplifiedSenators().filter(senator => senator[prop] === value)
+
+//console.log(filterSenators('gender', 'F'))
+
+const mostSeniorSenator = SimplifiedSenators().reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
+
+console.log(mostSeniorSenator)
+
+populateSenatorDiv(SimplifiedSenators())
